1f3971683b99763d7bbce5ab77ce78562fcc3552
DerSticher <robin@sticher.info>
2020-03-30 00:38:10 +0200

    fixed csv paths

--- js/coronachart.js
+++ js/coronachart.js
@@ -289,7 +289,7 @@ $(document).ready(function() {
         _coronaChart.update();
     });
 
-    Papa.parse("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv", 
+    Papa.parse("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", 
         {
             download: true,
             dynamicTyping: true,
@@ -306,7 +306,7 @@ $(document).ready(function() {
         }
     );
 
-    Papa.parse("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv", 
+    Papa.parse("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv", 
         {
             download: true,
             dynamicTyping: true,
@@ -319,7 +319,7 @@ $(document).ready(function() {
         }
     );
 
-    Papa.parse("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv", 
+    Papa.parse("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv", 
         {
             download: true,
             dynamicTyping: true,
